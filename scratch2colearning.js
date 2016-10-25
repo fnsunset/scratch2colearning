0c4c1796f99c8586410c0482c96e113e0ee88100
fnsunset <ponpon2904@gmail.com>
2016-10-25 14:20:01 +0900

    update

--- scratch2colearning.js
+++ scratch2colearning.js
@@ -1,7 +1,7 @@
 (function(ext) {
     var socket = io.connect('http://192.168.2.104:8080');
-	var $window = $(window);
-	var $x = $("#x"), $y = $("#y");
+	//var $window = $(window);
+	//var $x = $("#x"), $y = $("#y");
     // shutdown時に呼ばれる
     ext._shutdown = function() {};
 
@@ -19,19 +19,21 @@
     // ブロックと関数のひも付け
     var descriptor = {
         blocks: [
-            [' ', '%s に接続する', 'Obj_move', '10'],
-            [' ', '%n 歩動かす', 'Obj_move', '10'],
-            [' ', '時計回りに %n 度回す', 'Obj_move', '15'],
-            [' ', '反時計回りに %n 度回す', 'Obj_move', '15'],
-            [' ', '%n 度に向ける', 'Obj_move', '10'],
-            [' ', 'x座標を %n ずつ変える', 'Obj_move', '10'],
-            [' ', 'ｙ座標を %n ずつ変える', 'Obj_move', '10'],
-            ['r', '%m.List_member さんの %m.List_obj のx座標', 'Obj_move', ''],
-            ['r', '%m.List_member さんの %m.List_obj のy座標', 'Obj_move', '']
+            [' ', '%s に接続する', 'Obj_move', '192.168.2.104'],
+            [' ', '%m.List_obj を %n 歩動かす', 'Obj_move', '','10'],
+            [' ', '%m.List_obj を時計回りに %n 度回す', 'Obj_move', '', '15'],
+            [' ', '%m.List_obj を反時計回りに %n 度回す', 'Obj_move', '', '15'],
+            [' ', '%m.List_obj を %n 度に向ける', 'Obj_move', '', '10'],
+            [' ', '%m.List_obj のx座標を %n ずつ変える', 'Obj_move', '', '10'],
+            [' ', '%m.List_obj のｙ座標を %n ずつ変える', 'Obj_move', '', '10'],
+            ['r', '%m.List_member さんの %m.List_obj のx座標', 'Obj_move', '', ''],
+            ['r', '%m.List_member さんの %m.List_obj のy座標', 'Obj_move', '', ''],
+            ['h', '%s を受け取ったとき','Hello'],
+            [' ','%s を送る','Hello']
         ],
         menus:{
             List_member: ['A','B','C','D'],
-            List_obj:['x座標','y座標']
+            List_obj:['ねこ','モモンガ']
         }
     };
 
