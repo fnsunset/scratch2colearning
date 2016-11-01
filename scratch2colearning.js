f4e2b2b0a743edcdd236c7e83ded3a9ca88b1049
fnsunset <ponpon2904@gmail.com>
2016-11-01 23:10:14 +0900

    update

--- scratch2colearning.js
+++ scratch2colearning.js
@@ -1,6 +1,8 @@
 (function(ext) {
     var socket = io.connect('http://192.168.2.104:8080');
     var socket_id = '';
+    var members = ['A','B','C','D'];
+    var objects = ['ねこ','モモンガ','カエル'];
     socket.on('connect', function() { 
         socket_id = socket.id;
      });
@@ -13,12 +15,14 @@
     };
     //サーバ側から受信した情報を反映させる
     socket.on('server/hello', function (data) {
-        alert('Connect Success! ID:'+socket_id );
+        socket.emit('scratch/hello', { id: socket_id });
 	});
     // blockが呼び出された時に呼ばれる関数を登録する。
     // 下にあるdescriptorでブロックと関数のひも付けを行っている。
     ext.Obj_connect = function(str) {
-        socket.emit('scratch/connect', { ip: str, id: socket_id });
+        socket.disconnets();
+        var socket = io.connect('http://192.168.2.104:8080');
+        alert('Now you connected '+ str);
     };
     ext.Obj_move = function(str,num) {
         socket.emit('scratch/move', { obj: str, move: num, id: socket_id });
@@ -36,7 +40,10 @@
         socket.emit('scratch/movex', { obj: str, movex: num, id: socket_id });
     };
     ext.Obj_movey = function(str,num) {
-        socket.emit('scratch/movey', {  obj: str, movey: num, id: socket_id  });
+        socket.emit('scratch/movey', { obj: str, movey: num, id: socket_id });
+    };
+    ext.Obj_movey = function(str,num1,num2) {
+        socket.emit('scratch/warp', { obj: str, warpx: num1, warpy: num2, id: socket_id });
     };
     ext.Obj_getx = function(str1,str2) {
         socket.emit('scratch/getx', { x: 1, y: 1 });
@@ -53,21 +60,22 @@
     // ブロックと関数のひも付け
     var descriptor = {
         blocks: [
-            [' ', '%s に接続する', 'Obj_connect', '192.168.2.104'],
-            [' ', '%m.List_obj を %n 歩動かす', 'Obj_move', '',10],
-            [' ', '%m.List_obj を時計回りに %n 度回す', 'Obj_cw', '', 15],
-            [' ', '%m.List_obj を反時計回りに %n 度回す', 'Obj_rcw', '', 15],
-            [' ', '%m.List_obj を %n 度に向ける', 'Obj_ang', '', 10],
-            [' ', '%m.List_obj のx座標を %n ずつ変える', 'Obj_movex', '', 10],
-            [' ', '%m.List_obj のｙ座標を %n ずつ変える', 'Obj_movey', '', 10],
-            ['r', '%m.List_member さんの %m.List_obj のx座標', 'Obj_getx', '', ''],
-            ['r', '%m.List_member さんの %m.List_obj のy座標', 'Obj_gety', '', ''],
+            [' ', '%s に接続する', 'Obj_connect', '192.168.2.104:8080'],
+            [' ', '%m.List_obj を %n 歩動かす', 'Obj_move', 'ねこ',10],
+            [' ', '%m.List_obj を時計回りに %n 度回す', 'Obj_cw', 'ねこ', 15],
+            [' ', '%m.List_obj を反時計回りに %n 度回す', 'Obj_rcw', 'ねこ', 15],
+            [' ', '%m.List_obj を %n 度に向ける', 'Obj_ang', 'ねこ', 10],
+            [' ', '%m.List_obj のx座標を %n ずつ変える', 'Obj_movex', 'ねこ', 10],
+            [' ', '%m.List_obj のｙ座標を %n ずつ変える', 'Obj_movey', 'ねこ', 10],
+            [' ', '%m.List_obj のx座標を %n y座標を %n にする', 'Obj_warp', 'ねこ', 0, 0],
+            ['r', '%m.List_member さんの %m.List_obj のx座標', 'Obj_getx', 'A', 'ねこ'],
+            ['r', '%m.List_member さんの %m.List_obj のy座標', 'Obj_gety', 'A', 'ねこ'],
             ['h', '%s を受け取ったとき','Obj_res','Hello'],
             [' ','%s を送る','Obj_move','Hello']
         ],
         menus:{
-            List_member: ['A','B','C','D'],
-            List_obj:['ねこ','モモンガ','カエル']
+            List_member: members,
+            List_obj:objects
         }
     };
 
