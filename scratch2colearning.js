cc2d1c731ac8e17d9ea750fd221960844d718542
fnsunset <ponpon2904@gmail.com>
2016-11-25 16:34:58 +0900

    test

--- scratch2colearning.js
+++ scratch2colearning.js
@@ -304,7 +304,7 @@
             [' ', 'Connect %s', 'Connect','192.168.2.104'],
             ['r', 'Socket ID', 'Obj_getid'],
             ['r', 'emit', 'Obj_emit'],
-            [' ', '%m.List_obj を %n 歩動かす', 'Obj_move', list_obj[0],10],
+            ['w', '%m.List_obj を %n 歩動かす', 'Obj_move', list_obj[0],10],
             [' ', '%m.List_obj を時計回りに %n 度回す', 'Obj_cw', list_obj[0], 15],
             [' ', '%m.List_obj を反時計回りに %n 度回す', 'Obj_rcw', list_obj[0], 15],
             [' ', '%m.List_obj を %n 度に向ける', 'Obj_ang', list_obj[0], 10],
