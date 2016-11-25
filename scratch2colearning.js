3f6edddb459323c482f51e0a58236a7ac291249f
fnsunset <ponpon2904@gmail.com>
2016-11-25 22:03:15 +0900

    40-3

--- scratch2colearning.js
+++ scratch2colearning.js
@@ -26,8 +26,8 @@
     var send_log = [];
     var send_log_old = [];
     var execution = [];
-    var log_size = 4;
-    var wait_time = 35;
+    var log_size = 3;
+    var wait_time = 40;
     for(var cnta = 0; cnta < log_size; cnta++){
                 send_log[cnta] = [];
     }
