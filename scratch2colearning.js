27d1fcce0edaf4604b9166661c28ac7881aa94a4
fnsunset <ponpon2904@gmail.com>
2016-11-25 17:13:51 +0900

    test

--- scratch2colearning.js
+++ scratch2colearning.js
@@ -133,7 +133,7 @@
     }
     var checkJSONarray = function(object,objectarray){
         if(objectarray){
-            for(var cnt = 1; cnt < objectarray.length;cnt++){
+            for(var cnt = 0; cnt < objectarray.length;cnt++){
                 if(checkJSON(object,objectarray[cnt])){
                     return true;
                 }
