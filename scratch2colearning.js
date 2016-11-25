4402ab0edded1161b79f77158d29d80d5f331daf
fnsunset <ponpon2904@gmail.com>
2016-11-25 18:38:35 +0900

    teat

--- scratch2colearning.js
+++ scratch2colearning.js
@@ -92,7 +92,7 @@
             }
         });
         $.each(send_log[0],function(i,val){
-            if(!checkJSONarray(val,send_log_old)){
+            if(checkJSONarray(val,send_log_old)){}else{
                 //socket.emit(val.emit, {obj: val.obj, num1: val.num1, num2: val.num2, id: val.id, str: val.str, emitsw: 0});
                 for(var cnt = 0; cnt < execution.length;cnt++){
                     if(checkJSON(val,execution[cnt])){
