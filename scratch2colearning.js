5a1948fdcc8adfa9d06eef29fc93c1cd32a95e4e
fnsunset <ponpon2904@gmail.com>
2016-11-25 19:58:31 +0900

    tet

--- scratch2colearning.js
+++ scratch2colearning.js
@@ -118,7 +118,7 @@
         });
         $.each(send_log[0],function(i,val){
             if(!checkJSONarray(val,send_log_old)){
-                console.log(send_log_old.length);
+                console.log(JSON.stringify(send_log[0])+' \n'+JSON.stringify(send_log_old));
                 //socket.emit(val.emit, {obj: val.obj, num1: val.num1, num2: val.num2, id: val.id, str: val.str, emitsw: 0});
                 for(var cnt = 0; cnt < execution.length;cnt++){
                     if(checkJSON(val,execution[cnt])){
